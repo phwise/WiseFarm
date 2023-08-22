4262dcdd015dd0332aeb71f77df0cd05c9f40c69
phwise <phmkize@gmail.com>
2023-08-22 23:20:47 +0200

    first commit

---
+++ src/main/java/com/wisefarm/WisefarmApplication.java
@@ -0,0 +1,13 @@
+package com.wisefarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisefarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisefarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wisefarm/WisefarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisefarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisefarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
